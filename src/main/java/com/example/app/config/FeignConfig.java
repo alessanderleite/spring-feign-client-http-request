d5039f2a4e612754302f9a0d4298d630dc362749
Alessander <alessander.logistica@gmail.com>
2020-05-27 00:21:06 -0300

    add @Configuration Annotation

--- src/main/java/com/example/app/config/FeignConfig.java
+++ src/main/java/com/example/app/config/FeignConfig.java
@@ -1,9 +1,11 @@
 package com.example.app.config;
 
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
 
 import com.example.app.integration.TodoClient;
 
+@Configuration
 @EnableFeignClients(clients = {TodoClient.class})
 public class FeignConfig {
 
